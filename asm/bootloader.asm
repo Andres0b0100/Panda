27f0a6a10cd0f8fe1796817cd63f5e3d02c26d3d
Andres Goncalves <ag0b0100@gmail.com>
2018-01-25 16:28:28 -0400

    0.2: A Hello World Boot Loader.

--- asm/bootloader.asm
+++ asm/bootloader.asm
@@ -1,4 +1,35 @@
-; Fill 510 Bytes With 0's
-	times 0x200-0x2 db 0x0
-; Put The Signature At The End Of The Sector
+; The Boot Loader Segment
+	mov ax, 0x7C0
+		mov ds,ax
+		
+; Message To Print
+	mov si, msg
+; Call The Procedure
+	call puts
+
+; Loop Infinitely
+	jmp $
+
+msg db "Hello World!",0x0
+
+puts:
+; Load A Character
+	lodsb
+; Return If It's 0
+	test al,al
+	jz .end
+; Function #14: Teletype Character
+	mov ah, 0xE
+; First Video Page
+	mov bh, 0x0
+; BIOS Video Interrupt
+	int 0x10
+; Next Character
+	jmp puts
+.end:
+; Exit From The Function
+	ret
+
+; Fill The Rest Of The Sector
+	times 0x200-0x2-($-$$) db 0x0
 	dw 0xAA55
