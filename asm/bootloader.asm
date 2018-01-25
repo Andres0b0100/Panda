f53a1fef22cc0e01c59fbfd2c014f5079894e7a9
Andres Goncalves <ag0b0100@gmail.com>
2018-01-25 17:39:05 -0400

    0.3: A Two-Stage Boot Loader.

--- asm/bootloader.asm
+++ asm/bootloader.asm
@@ -1,35 +1,51 @@
-; The Boot Loader Segment
+first:
 	mov ax, 0x7C0
 		mov ds,ax
+		mov es,ax
 		
-; Message To Print
-	mov si, msg
-; Call The Procedure
+	mov si, .msg
 	call puts
 
-; Loop Infinitely
-	jmp $
+; Function #2: Read Disk	
+	mov ah, 0x2
+; Sectors To Read
+	mov al, 0x1
+; The Second Stage Offset
+	mov bx, second
+; Cylinder Number
+	mov ch, 0x0
+; Sector Number
+	mov cl, 0x2
+; Head Number
+	mov dh, 0x0
+; BIOS Disk Interrupt
+	int 0x13
+	
+; Jump To The Second Stage
+	jmp second
 
-msg db "Hello World!",0x0
+.msg db "First Stage.",0xD,0xA,0x0
 
 puts:
-; Load A Character
 	lodsb
-; Return If It's 0
 	test al,al
 	jz .end
-; Function #14: Teletype Character
 	mov ah, 0xE
-; First Video Page
 	mov bh, 0x0
-; BIOS Video Interrupt
 	int 0x10
-; Next Character
 	jmp puts
 .end:
-; Exit From The Function
 	ret
 
-; Fill The Rest Of The Sector
-	times 0x200-0x2-($-$$) db 0x0
+	times 0x200-0x2-($-first) db 0x0
 	dw 0xAA55
+	
+second: 
+	mov si, .msg
+	call puts
+	
+	jmp $
+	
+.msg db "Second Stage.",0x0
+; Fill Another Sector
+	times 0x200-($-second) db 0x0
